a4f7feda79b37125adeb6b2837787aa09b0eaee5
Patrick Nilan <nilan.patrick@gmail.com>
2023-09-18 15:42:36 -0700

    Added user, place, and list routes to routes.js

--- routes.js
+++ routes.js
@@ -1,7 +1,24 @@
-const routes = require('express').Router();
-const controllers = require('./controllers');
+const router = require('express').Router();
+const { userController, placeController, listController } = require('./controllers');
 
-// === Add routes here ===
+// User Routes
+router.get('/users', userController.getUsers);
+router.get('/users/:user_id', userController.getUserById);
+router.post('/users', userController.createUser);
+router.put('/users/:user_id', userController.updateUser);
+router.delete('/users/:user_id', userController.deleteUser);
+router.get('/users/:user_id/lists', userController.getUserLists);
+
+// Place Routes
+router.get('/places', placeController.getPlaces);
+router.get('/places/:place_id', placeController.getPlacesById);
+
+// List Routes
+router.get('/lists', listController.getLists);
+router.get('/lists/:list_id', listController.getListById);
+router.post('/lists', listController.createList);
+router.post('/lists/:list_id/spots', listController.addSpotToListById);
+router.delete('/list/:list_id/spots/:spot_id', listController.deleteSpotById)
 
 module.exports = routes;
 
