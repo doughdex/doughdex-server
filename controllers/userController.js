237b4e1872b47d6222b13c60836237e81bc7a1c6
Patrick Nilan <nilan.patrick@gmail.com>
2023-09-21 10:01:09 -0700

    GET /api/users endpoint should now work properly

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,32 @@
 const { userModel } = require('../models');
 const { decodeToken } = require('../middleware');
+const { createPaginationLinks } = require('./helpers');
 
-const getUsers = () => {
+const getUsers = async (req, res) => {
+  try {
+    const page = parseInt(req.query?.page) || 1;
+    const limit = parseInt(req.query?.limit) || 10;
+    const result = await userModel.getUsers(page, limit);
 
+    const data = result.rows;
+    const totalCount = parseInt(data[0].total_count, 10);
+    const totalPages = Math.ceil(totalCount / limit);
+    const links = createPaginationLinks(req, page, limit, totalPages);
+
+    const response = {
+      page,
+      limit,
+      totalCount,
+      totalPages,
+      links,
+      data
+    }
+
+    res.status(200).send(response);
+  } catch (error) {
+    console.error('Error retrieving users data:', error);
+    res.status(501).json({ message: 'Internal server error' });
+  }
 };
 
 const getUserById = () => {
--- models/user.js
+++ models/user.js
@@ -1,7 +1,14 @@
-const { db } = require('../db');
+const db = require('../db');
+const { setOffset } = require('./helpers');
 
-const getUsers = () => {
+const getUsers = (page, limit) => {
+  const offset = setOffset(page, limit);
+  const usersQuery = {
+    text: 'SELECT id, name, display_name, email, location, bio, avatar_url, COUNT(*) OVER() as total_count FROM users WHERE is_private = false AND is_banned = false LIMIT $1 OFFSET $2',
+    values: [limit, offset]
+  }
 
+  return db.query(usersQuery);
 };
 
 const getUserById = () => {
