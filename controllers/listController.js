8ed037af8bef34ab9419f0f84ead213ea3ed5d51
Patrick Nilan <nilan.patrick@gmail.com>
2023-10-03 12:00:59 -0700

    Created GET /api/lists and /api/lists/:list_id controller and model functions

--- controllers/listController.js
+++ controllers/listController.js
@@ -1,12 +1,42 @@
 const { listModel } = require('../models');
 const { decodeToken } = require('../middleware');
+const { createPaginationLinks } = require('./helpers');
 
-const getLists = () => {
+const getLists = async (req, res) => {
+  try {
+    const page = parseInt(req.query?.page) || 1;
+    const limit = parseInt(req.query?.limit) || 10;
+    const result = await listModel.getLists(page, limit);
+    const data = result.rows;
+    const totalCount = parseInt(data[0].total_count, 10);
+    const totalPages = Math.ceil(totalCount / limit);
+    const links = createPaginationLinks(req, page, limit, totalPages);
 
+    const response = {
+      page,
+      limit,
+      totalCount,
+      totalPages,
+      links,
+      data
+    };
+    res.status(200).send(response);
+  } catch (error) {
+    console.error('Error retrieving lists:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 };
 
-const getListById = () => {
-
+const getListById = async (req, res) => {
+  // Todo: If a list is private, only return if requested by list creator
+  try {
+    const result = await listModel.getListById(req.params.list_id);
+    const data = result.rows[0];
+    res.status(200).send(data);
+  } catch (error) {
+    console.error('Error retrieving list:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 };
 
 const createList = () => {
--- models/list.js
+++ models/list.js
@@ -1,11 +1,48 @@
 const { db } = require('../db');
+const { setOffset } = require('./helpers');
 
-const getLists = () => {
-
+const getLists = (page, limit) => {
+  const offset = setOffset(page, limit);
+  const query = {
+    text: 'SELECT id, user_id, name, created_at FROM lists WHERE is_private = false AND is_visible = TRUE AND is_flagged = false ORDER BY created_at DESC LIMIT $1 OFFSET $2',
+    values: [limit, offset]
+  };
+  return db.query(query);
 };
 
-const getListById = () => {
-
+const getListById = (id) => {
+  const query = {
+    text: ```
+      SELECT
+        l.id AS list_id,
+        l.user_id,
+        l.name AS list_name,
+        l.is_ordered,
+        l.is_private,
+        l.created_at AS list_created_at,
+        p.id AS place_id,
+        p.google_places_id,
+        p.name AS place_name,
+        p.address,
+        p.city,
+        p.state,
+        p.zip,
+        p.loc,
+        p.recommendations,
+        p.ratings_counts,
+        p.is_operational,
+        p.is_archived AS place_is_archived,
+        p.flagged AS place_flagged,
+        p.created_at AS place_created_at,
+        p.updated_at AS place_updated_at,
+        p.archived_at AS place_archived_at
+      FROM lists AS l
+      JOIN places AS p ON l.id = p.list_id
+      WHERE l.id = $1 AND l.is_private = false AND l.is_flagged = false AND l.is_visible = true;
+    ```,
+    values: [id]
+  }
+  return db.query(query);
 };
 
 const createList = () => {
