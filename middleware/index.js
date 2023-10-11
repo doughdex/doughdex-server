0e14c23f52881f329106a6b14592782c9dfd4783
Patrick Nilan <nilan.patrick@gmail.com>
2023-10-11 12:39:53 -0700

    Added isAdmin, isAuthenticated, and validateUserOwner middleware functions

--- middleware/index.js
+++ middleware/index.js
@@ -1,5 +1,37 @@
 const { authenticateRequestor } = require('./auth');
 
+const isAuthenticated = (req, res, next) => {
+
+  if (req.user) {
+    return next();
+  } else {
+    res.status(401).json({ message: 'Login required.' });
+  }
+};
+
+const validateUserOwnership = (req, res, next) => {
+
+  const userId = req.params.user_id;
+
+  if (req.user.id === userId) {
+    return next();
+  } else {
+    res.status(401).json({ message: 'Unauthorized' });
+  }
+};
+
+const isAdmin = (req, res, next) => {
+  if (req.user.is_admin) {
+    return next();
+  } else {
+    res.status(401).json({ message: 'Unauthorized' });
+  }
+};
+
+
 module.exports = {
   authenticateRequestor,
+  isAuthenticated,
+  validateUserOwnership,
+  isAdmin
 };
