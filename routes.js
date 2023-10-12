25ae4ef5dc48399d8a7f864ee01c179fb26d03c9
Patrick Nilan <nilan.patrick@gmail.com>
2023-10-12 11:01:59 -0700

    Added updated auth middleware to respective routes

--- routes.js
+++ routes.js
@@ -4,11 +4,11 @@ const middleware = require('./middleware');
 
 // User Routes
 router.get('/users', controllers.User.getUsers);
-router.get('/users/:user_id', controllers.User.getUserById);
+router.get('/users/:user_id', (req, res, next) => middleware.validateUserOwnership(req, res, next), controllers.User.getUserById);
 router.post('/users', controllers.User.createUser);
-router.put('/users/:user_id', (req, res, next) => middleware.authenticateUser(req, res, next), controllers.User.updateUser);
-router.delete('/users/:user_id', (req, res, next) => middleware.authenticateUser(req, res, next), controllers.User.deleteUser);
-router.get('/users/:user_id/lists', controllers.User.getUserLists);
+router.put('/users/:user_id', (req, res, next) => middleware.validateUserOwnership(req, res, next), controllers.User.updateUser);
+router.delete('/users/:user_id', (req, res, next) => middleware.validateUserOwnership(req, res, next), controllers.User.deleteUser);
+router.get('/users/:user_id/lists', (req, res, next) => middleware.validateUserOwnership(req, res, next), controllers.User.getUserLists);
 
 // Place Routes
 router.get('/places', controllers.Place.getPlaces);
@@ -16,11 +16,11 @@ router.get('/places/:place_id', controllers.Place.getPlaceById);
 
 // List Routes
 router.get('/lists', controllers.List.getLists);
-router.get('/lists/:list_id', controllers.List.getListById);
-router.post('/lists', (req, res, next) => middleware.authenticateUser(req, res, next), controllers.List.createList);
-router.post('/lists/:list_id/spots', (req, res, next) => middleware.authenticateUser(req, res, next), controllers.List.addSpotToList);
-router.delete('/list/:list_id/', (req, res, next) => middleware.authenticateUser(req, res, next), controllers.List.deleteList);
-router.delete('/list/:list_id/spots/:spot_id', (req, res, next) => middleware.authenticateUser(req, res, next), controllers.List.deleteSpotFromList);
+router.get('/lists/:list_id', (req, res, next) => middleware.validateUserOwnership(req, res, next), controllers.List.getListById);
+router.post('/lists', (req, res, next) => middleware.validateUserOwnership(req, res, next), controllers.List.createList);
+router.post('/lists/:list_id/spots', (req, res, next) => middleware.validateUserOwnership(req, res, next), controllers.List.addSpotToList);
+router.delete('/list/:list_id/', (req, res, next) => middleware.validateUserOwnership(req, res, next), controllers.List.deleteList);
+router.delete('/list/:list_id/spots/:spot_id', (req, res, next) => middleware.validateUserOwnership(req, res, next), controllers.List.deleteSpotFromList);
 
 // Admin Routes (FUTURE)
 
