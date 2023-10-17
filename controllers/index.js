30403ad2e215d8de9cd38eab94e28ae65a3213d4
Patrick Nilan <nilan.patrick@gmail.com>
2023-10-17 17:20:29 -0400

    Renamed admin file name

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,13 @@
 const User = require('./user');
 const Place = require('./place');
 const List = require('./list');
+const Admin = require('./admin');
 
 module.exports.controllers = {
   User,
   Place,
-  List
+  List,
+  Admin
 };
 
 
