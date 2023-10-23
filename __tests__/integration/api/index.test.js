534a9ba7a96fc721b05c2084490d7708f3a667a3
Patrick Nilan <nilan.patrick@gmail.com>
2023-10-23 15:51:20 -0700

    Added tests for invalid routes

--- __tests__/integration/api/index.test.js
+++ __tests__/integration/api/index.test.js
@@ -10,4 +10,12 @@ describe('/api/check', () => {
   it('returns success', async () => {
     await request(app).get('/api/check').expect(204);
   });
+
+  it('should handle invalid routes with api prefix', async () => {
+    await request(app).get('/api/invalid').expect(404);
+  });
+
+  it('should handle invalid routes', async () => {
+    await request(app).get('/invalid').expect(404);
+  });
 });
