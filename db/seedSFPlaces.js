e61449e6f9c3e1a9c6a3461189944787b5d24b1d
Patrick Nilan <nilan.patrick@gmail.com>
2023-10-24 16:59:53 -0700

    Updated function to seed database with pizza places

--- db/seedSFPlaces.js
+++ db/seedSFPlaces.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const path = require('path');
-const { Pool } = require('pg');
+const { Client } = require('pg');
 const fs = require('node:fs/promises');
+const data = require('./231024_clean_pizza_data.json');
 
-const pool = new Pool({
+const db = new Client({
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
@@ -13,14 +14,38 @@ const pool = new Pool({
 
 const seedSFPlaces = async () => {
   try {
-    const data = await fs.readFile('./clean_pizza_data.json');
-    const pizzaData = JSON.parse(data);
     await db.connect();
-    await Promise.all()
+
+    await db.query('DELETE FROM places')
+
+    for (const googlePlacesId in data) {
+      const { name, business_status, formatted_address, geometry } = data[googlePlacesId];
+
+      const is_operational = business_status === 'OPERATIONAL' ? true : false;
+
+      const addressParts = formatted_address.split(', ');
+
+      const address = addressParts[0];
+      const city = 'San Francisco';
+      const state = 'CA';
+      const zip = formatted_address.match(/\b9\d{4}\b/)[0];
+
+      const loc = `(${geometry.location.lng}, ${geometry.location.lat})`;
+
+      const query = {
+        text: 'INSERT INTO places (google_places_id, name, address, city, state, zip, loc, is_operational, is_approved) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
+        values: [googlePlacesId, name, address, city, state, zip, loc, is_operational, true]
+      };
+      await db.query(query);
+    }
 
   } catch (error) {
-    console.error('Error seeding initial SF pizza places');
+    console.error('Error seeding initial SF pizza places', error);
+  } finally {
+    await db.end();
   }
 
 
 };
+
+seedSFPlaces();
